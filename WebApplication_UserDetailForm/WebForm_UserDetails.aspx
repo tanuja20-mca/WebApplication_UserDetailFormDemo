438332464eb41da2cdd8125052b9fe0d34a7c024
Dell <Dell@DESKTOP-RN29GV5>
2022-02-07 18:04:58 +0530

    Add project files.

---
+++ WebApplication_UserDetailForm/WebForm_UserDetails.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm_UserDetails.aspx.cs" Inherits="WebApplication_UserDetailForm.WebForm_UserDetails" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-left: 40px">
+            <asp:Label ID="lblName" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="lblLocation" runat="server" Text="Location"></asp:Label>
+            <asp:ListBox ID="lstLocation" runat="server" AutoPostBack="True" OnSelectedIndexChanged="lstLocation_SelectedIndexChanged">
+                <asp:ListItem>Nainital</asp:ListItem>
+                <asp:ListItem>Haldwani</asp:ListItem>
+                <asp:ListItem>Almora</asp:ListItem>
+                <asp:ListItem>Delhi</asp:ListItem>
+            </asp:ListBox>
+            <br />
+            <br />
+            <asp:Label ID="lblGender" runat="server" Text="Gender"></asp:Label>
+            <asp:RadioButton ID="rdbtnFemale" runat="server" Text="Female" GroupName="gender" />
+            <asp:RadioButton ID="rdbtnMale" runat="server" Text="Male" GroupName="gender" />
+            <asp:RadioButton ID="rdbtnOther" runat="server" Text="Other" GroupName="gender"/>
+            <br />
+            <br />
+            <asp:Label ID="lblCourse" runat="server" Text="Course"></asp:Label>
+            <asp:CheckBox ID="chkJava" runat="server" Text="Java" />
+            <asp:CheckBox ID="chkCs" runat="server" Text="C#" />
+            <asp:CheckBox ID="chkReact" runat="server" Text="React" />
+            <asp:CheckBox ID="chkPython" runat="server" Text="Python" />
+            <br />
+        </div>
+        <div style="margin-left: 80px">
+            <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Submit" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication_UserDetailForm/WebForm_UserDetails.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication_UserDetailForm
+{
+    public partial class WebForm_UserDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lstLocation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Response.Write(txtName.Text +"</br>");
+            Response.Write(lstLocation.SelectedItem.Text +"</br>");
+            var gender = "";
+            if (rdbtnFemale.Checked)
+            {
+                gender = "Female";
+            }
+            else if (rdbtnMale.Checked)
+            {
+                // lblGender.Text = rdbtnMale.Text;
+                gender = "Male";
+            }
+            else
+            {
+               // lblGender.Text = rdbtnOther.Text;
+                gender = "Others";
+            }
+
+           
+            Response.Write(gender + "</br>");
+
+            var courses = "";
+            if (chkJava.Checked)
+            {
+                courses = chkJava.Text + " ";
+            }
+            if (chkCs.Checked)
+            {
+                courses += chkCs.Text + " ";
+            }
+            if (chkReact.Checked)
+            {
+                courses += chkReact.Text;
+            }
+            if (chkPython.Checked)
+            {
+                courses += chkPython.Text;
+            }
+           
+            lblCourse.Text = courses;
+            Response.Write(courses + "</br>");
+
+
+            lblName.Visible = false;
+            txtName.Visible = false;
+            lblLocation.Visible = false;
+            lstLocation.Visible = false;
+            lblGender.Visible= false;
+            rdbtnFemale.Visible = false;
+            rdbtnFemale.Checked=false;
+            rdbtnMale.Visible = false;
+            rdbtnMale.Checked=false;
+            rdbtnOther.Visible = false;
+            rdbtnOther.Checked=false;
+            lblCourse.Visible=false;
+            chkJava.Visible = false;
+            chkJava.Checked=false;
+            chkCs.Visible = false;
+            chkCs.Checked = false;
+            chkReact.Visible = false;
+            chkReact.Checked = false;
+            chkPython.Checked = false;
+            chkPython.Visible = false;
+            btnSubmit.Visible = false;
+            
+
+        }
+    }
+}
